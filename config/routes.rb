2921b6075630636ff0001a843b5a5c336305f4a6
Gabriel Shufelt <gabshu2014@gmail.com>
2025-02-01 17:26:43 -0500

    COOKINGG

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'home#index'
 
